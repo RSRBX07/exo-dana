56ac3dc279bfcaea90ea8cf1286dafa20366cae2
Ghanem <ghanemdana@hotmail.com>
2017-01-09 14:35:13 +0100

    exo2 nombres

--- numbers.rb
+++ numbers.rb
@@ -1,4 +1,9 @@
 # exercices sur les nombres: calcul nbre de sec en 1 année
 
 year= 365*24*60*60
-print year
+puts year
+
+# exercicsur les nombres: calcul de l'age en secondes
+
+age= 36*year
+puts age
