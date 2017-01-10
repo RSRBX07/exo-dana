086b0a4146501903a806b84e1b29f98abef0880b
Ghanem <ghanemdana@hotmail.com>
2017-01-10 14:57:49 +0100

    cagnote vendredi13

---
+++ loto2.rb
@@ -0,0 +1,10 @@
+# affichage du montant de la cagnote
+# montant entre 100 et 500 euros
+vendredi_13 = true
+if vendredi_13
+  cagnote= 2_000_000
+else 
+  cagnote= 100_1000
+end
+puts "le montant de la cagnote du jour est de : #{cagnote}"
+ 
