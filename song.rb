d3f22355ae552c1f4b6ca3a47bc785790bb2dbe0
Ghanem <ghanemdana@hotmail.com>
2017-01-10 16:49:05 +0100

    stage2

--- song.rb
+++ song.rb
@@ -1,7 +1,6 @@
 # UN CODE QUI 2CRIT LA CHANSON ALLONGEONS LA JAMBE
 
 chanson= "Ma poule a 30 poussins"
-poussins= 30
-nvx_poussins= poussins-1
-1..29 
+if poussins= 30
+1..29 "ref_text #{nvx_poussins= poussins-1} text"
 
