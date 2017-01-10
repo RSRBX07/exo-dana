e6b13406f465bab4274f4a83259c9e9459598d6e
Ghanem <ghanemdana@hotmail.com>
2017-01-10 15:08:25 +0100

    finish

--- loto.rb
+++ loto.rb
@@ -1,3 +1,15 @@
 # affiche le tirage loto de 5 valeurs comprises entre 1 et 45
-loto =[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,2245]
+rand1= Random.rand(1..45)
+rand2= Random.rand(1..45)
+rand3= Random.rand(1..45)
+rand4= Random.rand(1..45)
+rand5= Random.rand(1..45)
+
+puts rand1, rand2, rand3, rand4, rand5
+
+
+
+
+
+
 
