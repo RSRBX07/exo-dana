77ac3060462449bcb764966dc847342e300e4054
Ghanem <ghanemdana@hotmail.com>
2017-01-11 16:36:28 +0100

    loto avec grille jeux

--- loto2.rb
+++ loto2.rb
@@ -1,3 +1,12 @@
+# saisie des chiffres de l'utilisateur
+
+puts "Choissisez 5 chiffres compris entre 1 et 45"
+choix_loto= gets.to_i
+
+# Tirage cagnote
+Tirage= 5.Random.rand(1..45)
+puts Tirage
+
 # affichage du montant de la cagnote
 # montant entre 100 et 500 euros
 vendredi_13 = true
@@ -7,3 +16,4 @@ else
   cagnote= 100_1000
 end
 puts "le montant de la cagnote du jour est de : #{cagnote}"
+
--- song.rb
+++ song.rb
@@ -1,7 +1,6 @@
 # UN CODE QUI 2CRIT LA CHANSON ALLONGEONS LA JAMBE
 poussins= 30
-if poussins=30
-poussin= poussins-1
-(1..29).each do puts "blablabla #{poussin}"
-else puts "dernier blablabla"
+while poussins <= 30
+nvx_poussins=poussins-1
+puts "blablabla #{nvx_poussins}"
 end
