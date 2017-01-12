fcd255ce936c4c33ce40038efad3eb84fd483950
Ghanem <ghanemdana@hotmail.com>
2017-01-12 14:13:08 +0100

    French number

---
+++ French_number.rb
@@ -0,0 +1,10 @@
+# écrire une methode qui prend un fix num en argument et qui retourne le string du nombre "écrit en lettres"
+
+puts" donez un chiffre"
+french_number= gets.chomp.to_s
+
+def number french_number
+french_number
+end
+
+puts french_number
