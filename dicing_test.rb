679e0a5bb816b68dae6ded945cf35e1df3cf85c7
Ghanem <ghanemdana@hotmail.com>
2017-01-17 11:02:32 +0100

    private

--- dicing_test.rb
+++ dicing_test.rb
@@ -1,16 +1,17 @@
 
 require './loto.rb'
 
-loto_du_samedi = Loto.new
-2.times {loto_du_samedi.validate_grid Loto.get_flash}
+#loto_du_samedi = Loto.new
+#2.times {loto_du_samedi.check_grid Loto.get_flash}
 
 #.each { |grid| loto_du_samedi.check_grid grid }
 
 loto_du_lundi = Loto.new
-loto_du_lundi.validate_grid Loto.get_flash
+loto_du_lundi.check_grid Loto.get_grid
 #loto_du_lundi.check_grid(grid)
 if loto_du_lundi.has_winner?
-  puts "Someone win"
+  puts "Someone wins #{prize}"
 else
-  puts "nobody win"
+  puts "nobody wins"
 end
+
