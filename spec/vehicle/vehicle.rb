fc574967cbafcb6a6bf60e9f3e47c33cb189c6d0
Ghanem <ghanemdana@hotmail.com>
2017-01-19 11:38:17 +0100

    exo variable de classe

--- spec/vehicle/vehicle.rb
+++ spec/vehicle/vehicle.rb
@@ -1,15 +1,33 @@
 class Vehicle
 attr_reader :position
 attr_reader :out_of_order
-
+attr_reader :count
 
   def initialize
     @position = :roubaix
     @out_of_order= false
+    @count = 0
   end
 
   def move destination
     @position = destination
   end
 
+  def count
+    @count +1irb
+  end
+
 end
+
+puts Vehicle.count.inspect
+
+
+
+
+# return the number of existing objects in memory
+# open irb
+# > Vehicule.count
+# result would be 0
+# > Vehicle.new
+# > Vehicle.count
+# result would be 1
