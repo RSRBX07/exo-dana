d4a247a080054a4182743688c4a0d5e45d9cc333
Ghanem <ghanemdana@hotmail.com>
2017-01-19 14:25:51 +0100

    File Class methods
    (open (that takes 2 arguments) and each_line

--- spec/vehicule.rb
+++ spec/vehicule.rb
@@ -1,12 +1,51 @@
+
+
+
+class Counter
+
+  def add_one
+    new_val= value +1
+      File.open "./tmp/counter.txt", "w" do |counter_file|
+        counter_file.write new_val
+      end
+
+end
+
+
+
+  def value
+    File.open "./tmp/counter.txt", "r" do |counter_file|
+    counter_file.each_line {|line| return line.to_i}
+    p counter_file
+  end
+
+end
+
+
 class Vehicule
 attr_reader :position
+attr_reader :out_of_order
 
-Def initialize
-@position = "Roubaix"
-end
+  def initialize
+  @position = "Roubaix"
+  @out_of_order = false
+  end
 
-Def move
-puts "I'm moving"
-end
+
+  def initialize
+    puts "Im starting initialization my instance"
+    @out_of_order = false
+    puts "Im finishing initialization my instance"
+    Counter.new.add_one
+  end
+
+
+  def move
+  puts "I'm moving"
+  end
+
+  def self.count
+    Counter.new.value
+  end
 
 end
