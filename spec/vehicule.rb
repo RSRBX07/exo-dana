62d08b124a788b1c4ee20f6993998303db03ab7c
Ghanem <ghanemdana@hotmail.com>
2017-01-19 16:15:30 +0100

    with the right path...

--- spec/vehicule.rb
+++ spec/vehicule.rb
@@ -1,51 +1,57 @@
-
-
-
 class Counter
 
-  def add_one
-    new_val= value +1
-      File.open "./tmp/counter.txt", "w" do |counter_file|
-        counter_file.write new_val
-      end
-
-end
-
-
-
   def value
-    File.open "./tmp/counter.txt", "r" do |counter_file|
-    counter_file.each_line {|line| return line.to_i}
-    p counter_file
+    File.open file_path, "r" do |counter_file|
+      counter_file.each_line {|content| return content.to_i}
+    end
+  end
+
+  # def add_one 
+  #   @new_val= value +1
+  #     File.open "./tmp/counter.txt", "w" do |counter_file|
+  #       counter_file.write new_val
+  #     end
+  # end
+  private
+
+  def file_dir
+    File.join(File.dirname(__FILE__), '../tmp')
+  end
+
+  def file_path
+    Dir::mkdir file_dir unless File.exist? file_dir
+    file_dir + '/counter.txt'
   end
 
 end
 
+trial=  Counter.new
+puts "this is the content value #{trial.value}"
 
-class Vehicule
-attr_reader :position
-attr_reader :out_of_order
+# class Vehicule
+#   attr_reader :position
+#   attr_reader :out_of_order
 
-  def initialize
-  @position = "Roubaix"
-  @out_of_order = false
-  end
-
-
-  def initialize
-    puts "Im starting initialization my instance"
-    @out_of_order = false
-    puts "Im finishing initialization my instance"
-    Counter.new.add_one
-  end
+#   def initialize
+#   @position = "Roubaix"
+#   @out_of_order = false
+#   end
 
 
-  def move
-  puts "I'm moving"
-  end
+#   def initialize
+#     puts "Im starting initialization my instance"
+#     @out_of_order = false
+#     puts "Im finishing initialization my instance"
+#     Counter.new.add_one
+#   end
 
-  def self.count
-    Counter.new.value
-  end
 
-end
+#   def move
+#   puts "I'm moving"
+#   end
+
+#   def self.count
+#     Counter.new.value
+#   end
+
+# end
