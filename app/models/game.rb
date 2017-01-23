b0cc8b22d02500046cb69a592fa8b45c989bbd96
Ghanem <ghanemdana@hotmail.com>
2017-01-23 16:08:17 +0100

    with incrementation

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,6 +2,15 @@ class Game < ApplicationRecord
 
   def add_player
     @players +=1
+
+  end
+
+  def remove_player
+    raise RuntimeError.new "No more players can be added" if players <=0
+    @players -= 1
+    else
+      @players 
+    end
   end
 
   private 
