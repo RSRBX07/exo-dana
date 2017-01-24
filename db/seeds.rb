2c606b9b52ca7416ffb44b09b7674bad17337683
Ghanem <ghanemdana@hotmail.com>
2017-01-24 11:29:49 +0100

    loto model seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# this is the seed for a loto game 
+Game.destroy_all
+Game.create [
+{name: "Loto du lundi", description: "le tirage est 12,36,98,62 et 77", players: 2255957},
+{name: "Loto du mardi", description: "le tirage est 2,36,52 et 7", players: 2255957},
+{name: "Loto du mercredi", description: "le tirage est 65,36,98,72 et 77", players: 2255957},
+{name: "Loto du jeudi", description: "le tirage est 3,78,98,02 et 77", players: 2255957},
+{name: "Loto du vendredi", description: "le tirage est 96,36,98,62 et 31", players: 2255957},
+]
+
